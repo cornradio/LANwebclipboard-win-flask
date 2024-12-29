bb292f38e6605cb140eedf5a301ebd5966fa6e91
Name <kasusaland@gmail.com>
2024-12-29 10:35:37 +0800

    build faster

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ pyinstaller --name=apploader --onefile --noconsole apploader.py -y
 
 :: 使用 PowerShell 压缩文件
 echo Creating ZIP archive...
-powershell Compress-Archive -Path dist/* -DestinationPath Lan_clipboard_app.zip -Force
+powershell Compress-Archive -Path dist/* -DestinationPath Lan_clipboard_app_win.zip -Force
 
 :: ask for clean?
 set /p clean=Do you want to clean the build files? (y/n): 
